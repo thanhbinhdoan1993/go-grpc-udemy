8cc181007a970c7e906ddadcc9c99ec7bf844cf6
Brian <thanhbinhdoan1993@gmail.com>
2020-06-28 12:31:11 +0700

    add laptop serivce

---
+++ service/laptop_store.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/jinzhu/copier"
+
+	"github.com/thanhbinhdoan1993/go-grpc-udemy/pb"
+)
+
+var ErrAlreadyExists = errors.New("record already exists")
+
+// LaptopStore is an interface to store laptop
+type LaptopStore interface {
+	// Save saves the laptop to the store
+	Save(laptop *pb.Laptop) error
+}
+
+// InMemoryLaptopStore stores laptop in memory
+type InMemoryLaptopStore struct {
+	mutex sync.RWMutex
+	data  map[string]*pb.Laptop
+}
+
+// NewInMemoryLaptopStore returns a new InMemoryLaptopStore
+func NewInMemoryLaptopStore() *InMemoryLaptopStore {
+	return &InMemoryLaptopStore{
+		data: make(map[string]*pb.Laptop),
+	}
+}
+
+// Save saves the laptop to the store
+func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.data[laptop.Id] != nil {
+		return ErrAlreadyExists
+	}
+
+	// deep copy
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+
+	store.data[other.Id] = other
+	return nil
+}
